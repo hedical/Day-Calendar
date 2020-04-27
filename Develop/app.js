5759386bd42b35dbdc3318de5d932a46820897d5
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-04-26 19:59:35 -0700

    added local storage function

--- Develop/app.js
+++ Develop/app.js
@@ -3,9 +3,23 @@ $(document).ready(function () {
     var time = moment().format('H');
     var currentDay = moment().format('LLLL');
     var numActualHour = parseInt(time, 10);
-
-
     var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+    // need to fix the AM-PM sytem
+    // need to store values as 9 AM, 10 AM ... and find the matching one to the actual hour. Then all the one after the actual hour matching value go green, and other grey
+
+
+    $(document).on("click", "button", function () {
+        var index = parseInt($(this).attr("data-id"));
+        var textArea = $(`#text-${[index]}`).val();
+        var hour = "hour-" + index;
+        console.log(textArea);
+        window.localStorage.setItem(hour, textArea);
+        // showAlert("Succesfully edited todo", "success");
+        // renderTodos();
+
+    })
+
+
 
     function generateCurrentDay() {
         $("header").append(currentDay)
@@ -16,8 +30,8 @@ $(document).ready(function () {
         for (var i = 0; i < arr.length; i++) {
             $(".container").append(`<div id="hour-${arr[i]}" class="">
             <h6 class="align-center">${arr[i]}AM</h6>
-            <textarea name="dunno" id="" cols="100" rows="2"></textarea>
-            <button class="btn btn-primary">Save</button>
+            <textarea name="dunno" id="text-${arr[i]}" cols="100" rows="2"></textarea>
+            <button id= "btn-${arr[i]}" data-id= "${arr[i]}" class="btn btn-primary">Save</button>
           </div>`)
         }
     } generateCalendar(hours)
@@ -39,12 +53,7 @@ $(document).ready(function () {
 
     } setColorBlocks(hours)
 
-    function saveTasks(arr) {
-        for (var i = 0; i < arr.length; i++) {
-            window.localStorage()
-        }
 
 
-    }
 
 })
