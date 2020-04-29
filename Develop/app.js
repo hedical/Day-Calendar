a2f9a6fc518eea0f7c454d6bd9effa5c1f4b5a72
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-04-28 19:36:24 -0700

    fully functionnal version

--- Develop/app.js
+++ Develop/app.js
@@ -1,20 +1,14 @@
-// more features if possible :
-// - count how many task we have on the day
-// - no access to an hour already passed
-// - differentiate a block with a task and without (using a span for example)
-// - put a reset button to reset all tasks
-// - refresh time each second on header
-
-
 $(document).ready(function () {
 
-    // Setting up variables
+    // Setting up global variables
     var time = moment().format('H');
     var currentDay = moment().format('LLLL');
     var numActualHour = parseInt(time, 10);
     var count = 0;
     var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
+    //Hiding the alert
+    $("#alert").hide()
 
     // Save buttons to store in local storage
     $(document).on("click", "button", function () {
@@ -25,32 +19,41 @@ $(document).ready(function () {
         count = 0;
         $("#counter").html("");
         countTask()
-        // showAlert("Succesfully edited todo", "success");
-        // renderTodos();
+        showAlert("Task succesfuly added", "success")
+    })
 
+    // Reset button to clear local storage and reset the task counter
+    $("#resetBtn").on("click", function () {
+        window.localStorage.clear();
+        displayTask(hours)
+        count = 0;
+        $("#counter").html("");
+        countTask()
+        showAlert("All tasks deleted, see you tomorrow :)", "primary")
     })
 
 
     // Function to display the current day on top of the screen
     function generateCurrentDay() {
-        $("header").append(currentDay)
+        $("header").prepend(currentDay)
     }
     generateCurrentDay();
 
 
-    // Function to generate all the calendar blocks
+    // Function to generate all the calendar blocks depending on our "hours" array
     function generateCalendar(arr) {
         for (var i = 0; i < arr.length; i++) {
-            $(".container").append(`<div class="" id="hour-${arr[i]}">
-            <h6 class="text-center col-1">${arr[i]}H</h6>
-            <textarea class="text-center col-10" id="text-${arr[i]}" name="dunno" cols="100" rows="2"></textarea>
-            <button class="btn btn-info col-1" id="btn-${arr[i]}" data-id="${arr[i]}">Save</button>
+            $(".container").append(`
+            <div class="" id="hour-${arr[i]}">
+                <h6 class="text-center col-1 p-0">${arr[i]}H</h6>
+                <textarea class="text-center col-10" id="text-${arr[i]}" name="dunno" cols="100" rows="2"></textarea>
+                <button class="btn btn-info col-1 p-0" id="btn-${arr[i]}" data-id="${arr[i]}">Save</button>
           </div>`)
         }
     } generateCalendar(hours)
 
 
-    // function to set the attribute of each time block, depending on the current time
+    // Function to set the attribute of each time block, depending on the current time
     function setColorBlocks(arr) {
         for (var i = 0; i < arr.length; i++) {
             if (numActualHour > arr[i]) {
@@ -67,7 +70,7 @@ $(document).ready(function () {
     } setColorBlocks(hours)
 
 
-    // Get item from local storage on loading and counting the number of tasks
+    // Get item from local storage on loading
     function displayTask() {
 
         for (var i = 9; i < hours.length + 9; i++) { // 9 is the first hour of the day
@@ -89,23 +92,29 @@ $(document).ready(function () {
 
             var text = window.localStorage.getItem("hour-" + i);
             $(`#text-${[i]}`).val(text);
+
             if (text) {
                 count++
-                console.log(count);
-
             }
-        } $("#counter").text((`You have ${count} tasks to do`))
+
+        }
+        if (count === 1) {
+            $("#counter").text((`You have ${count} task to do`))
+        } else {
+            $("#counter").text((`You have ${count} tasks to do`))
+        }
 
     } countTask()
 
+    // NEW FEATURE : Show an alert
+    function showAlert(str, type) {
+        $("#alert").show();
 
-    function showAlert() {
+        $("#alert").attr("class", `alert alert-${type}`);
+        $("#alert").text(str);
+        window.setTimeout(function () {
+            $("#alert").hide();
+        }, 2000)
 
     }
-
-    // function reset(){
-
-    // }
-
-
 })
